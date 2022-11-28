86a44c25db7e317bcf13ee8df9bdf5f3f97677fb
Chris Krogh <christopher.krogh@faire.com>
2022-11-28 17:30:58 -0500

    updated css import

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from "next/app";
 
-import "./index.css";
+import "../src/components/GoogleMap/google-maps.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
